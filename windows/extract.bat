99f7499c51c7206fcf8c7e24e06fef4522a162e6
bsxf47 <bsxf47@gmail.com>
2020-12-20 14:47:38 +0100

    Windows: Show the path to extracted files at the end of the process

--- windows/extract.bat
+++ windows/extract.bat
@@ -4,6 +4,6 @@ set pathToScripts=%~dp0..
 echo %filepath%
 py %pathToScripts%\extract_mview.py %filepath%
 set dir=%filepath:~0,-6%
-echo %dir%
 py %pathToScripts%\extract_model.py %dir%
+echo Files extracted to %dir%
 pause
