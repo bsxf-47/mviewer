2a4935f2ea43855c46234f95326f0f98052f8385
bsxf47 <bsxf47@gmail.com>
2020-12-20 13:27:00 +0100

    Windows: Replace absolute paths with relative ones
    Now works regardless of where the project is extracted.

--- windows/extract.bat
+++ windows/extract.bat
@@ -1,9 +1,10 @@
 @echo off
 set filepath=%1
+set pathToScripts=%~dp0..
 echo %filepath%
-py F:\Downloads\mviewer-master\mviewer-master\extract_mview.py %filepath%
+py %pathToScripts%\extract_mview.py %filepath%
 set dir=%filepath:~0,-6%
 echo %dir%
 pause
-py F:\Downloads\mviewer-master\mviewer-master\extract_model.py %dir%
+py %pathToScripts%\extract_model.py %dir%
 pause null
